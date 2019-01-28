190d4465947c6073cb9b19a82d1aa38d8b7a505b
svn <svn@06ee2f96-5407-11de-8bab-f3d7589a4122>
2019-01-28 07:57:44 +0000

    Added A/D input test
    git-svn-id: svn+ssh://subversion.munts.net/src/trunk/ada/tutorials/AdaEurope2019/src@16710 06ee2f96-5407-11de-8bab-f3d7589a4122

---
+++ test_adc.adb
@@ -0,0 +1,34 @@
+-- USB HID Remote I/O A/D Converter Test
+
+WITH Ada.Text_IO; USE Ada.Text_IO;
+
+WITH ADC.RemoteIO;
+WITH Analog;
+WITH HID.hidapi;
+WITH RemoteIO.Client;
+WITH Voltage;
+
+PROCEDURE test_adc IS
+
+  remdev : RemoteIO.Client.Device;
+  AIN0   : Voltage.Interfaces.Input;
+
+BEGIN
+  New_Line;
+  Put_Line("USB HID Remote I/O A/D Converter Test");
+  New_Line;
+
+  -- Open the remote I/O device
+
+  remdev := RemoteIO.Client.Create(HID.hidapi.Create);
+  AIN0   := ADC.Create(remdev.Create(1), 3.3);
+
+  -- Display analog input voltage
+
+  LOOP
+    Voltage.Volts_IO.Put(AIN0.Get);
+    New_Line;
+
+    DELAY 1.0;
+  END LOOP;
+END test_adc;
